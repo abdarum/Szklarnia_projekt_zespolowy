53858e24bfff517bcb357dd01692b4311ef5d146
Kornel Stefańczyk <kornel@kornel.laptop>
2019-05-15 18:08:11 +0200

    README hardware

---
+++ Hardware/Schematy/README.md
@@ -0,0 +1,7 @@
+# Schematy hardware:
+
+## Schemat elektroniczny:
+
+Projekt elektroniczny został stworzony w środowisku EasyEda, link do projektu:
+https://easyeda.com/stefanczykdk/projekt-zespo-owy-parapetowa-szklarnia 
+
---
+++ move_schematic.sh
@@ -0,0 +1,3 @@
+mv ~/Pobrane/Schemat_blokowy_hardware.drawio ~/Programowanie/GitHub/Szklarnia_projekt_zespolowy/Hardware/Schematy/
+
+mv ~/Pobrane/Schemat_blokowy_hardware.png ~/Programowanie/GitHub/Szklarnia_projekt_zespolowy/Hardware/Schematy/
