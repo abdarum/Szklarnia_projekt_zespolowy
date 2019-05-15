a470101a07a7d31551b8071c5260c78b88841b1d
Kornel Stefańczyk <kornel@kornel.laptop>
2019-05-15 18:09:56 +0200

    removed

--- move_schematic.sh
+++
@@ -1,3 +0,0 @@
-mv ~/Pobrane/Schemat_blokowy_hardware.drawio ~/Programowanie/GitHub/Szklarnia_projekt_zespolowy/Hardware/Schematy/
-
-mv ~/Pobrane/Schemat_blokowy_hardware.png ~/Programowanie/GitHub/Szklarnia_projekt_zespolowy/Hardware/Schematy/
